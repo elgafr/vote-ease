b62061f4a7ef4e3033c6281854fd8a08158ca18f
elgafr <elgaputrifr@gmail.com>
2025-01-23 14:01:38 +0700

    add login user api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,49 +1,86 @@
-import jwt from "jsonwebtoken"
-import User from "../models/User.js"
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",})
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
 
-export const registerUser = async(req, res) => {
+export const registerUser = async (req, res) => {
+  const { fullName, username, email, password, profileImageUrl } = req.body;
 
-    const { fullName, username, email, password, profileImageUrl } = req.body
+  if (!fullName || !username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    if(!fullName || !username || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" })
+  const usernameRegex = /^[a-zA-Z0-9]+$/;
+  if (!usernameRegex.test(username)) {
+    return res.status(400).json({ message: "Username must be alphanumeric" });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email alredy exists" });
     }
 
-    const usernameRegex = /^[a-zA-Z0-9]+$/;
-    if(!usernameRegex.test(username)) {
-        return res.status(400).json({ message: "Username must be alphanumeric" })
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res
+        .status(400)
+        .json({
+          message: "Username not available. Please choose another one.",
+        });
     }
 
-    try {
-        const existingUser = await User.findOne({ email });
-        if(existingUser) {
-            return res.status(400).json({ message: "Email alredy exists" })
-        }
+    const user = await User.create({
+      fullName,
+      username,
+      email,
+      password,
+      profileImageUrl,
+    });
 
-        const existingUsername = await User.findOne({ username })
-        if(existingUsername) {
-            return res.status(400).json({ message: "Username not available. Please choose another one." })
-        }
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
 
-        const user = await User.create({
-            fullName,
-            username,
-            email,
-            password,
-            profileImageUrl,
-        })
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-        res.status(201).json({
-            id: user._id,
-            user,
-            token: generateToken(user._id),
-        })
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong", error: error.message })
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email })
+    if(!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" })
     }
-}
+
+    res.status(200).json({
+      id: user._id,
+      user: {
+        ...user.toObject(),
+        totalVotesCreated: 0,
+        totalCastVotes: 0,
+        totalVotesBookmarked: 0,
+      },
+      token: generateToken(user._id),
+    })
+  } catch(error) {
+    res.status(500).json({ message: "Error registering user", error: error.message })
+  }
+
+};
+
+export const getUserInfo = async (req, res) => {};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,10 @@
 import express from "express"
-import { registerUser } from "../controllers/authController.js"
+import { registerUser, loginUser, getUserInfo } from "../controllers/authController.js"
+import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.get("/getUser", protect, getUserInfo)  
 
 export default router
